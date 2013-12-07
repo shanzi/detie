e12f07d36a3a53531bb9834a0a74b46bbcf39a2b
Chase Zhang <yun.er.run@gmail.com>
2013-12-07 17:59:25 +0800

    Change alogrim

--- detie/prob/__init__.py
+++ detie/prob/__init__.py
@@ -7,23 +7,9 @@ from detie.prob.prob_trans import P as trans
 
 MIN_FLOAT=0
 
-_prob_trans_BE = trans['B'].get('E', MIN_FLOAT)
-_prob_trans_MM = trans['M'].get('M', MIN_FLOAT)
-_prob_trans = trans['B'].get('M', MIN_FLOAT) + trans['M'].get('E',MIN_FLOAT)
-_prob_start = start.get('B')
-
 def word_prob(text):
     len_ = len(text)
-    if len_ <= 1: return MIN_FLOAT
+    if len_ <= 1: return emit['S'].get(text, MIN_FLOAT)
     prob_b = emit['B'].get(text[0], MIN_FLOAT)
     prob_e = emit['E'].get(text[-1], MIN_FLOAT)
-    print prob_b
-    print prob_e
-    if len_ == 2:
-        return _prob_start + prob_b + prob_e + _prob_trans_BE
-    else:
-        prob_m = sum(emit.get(char, MIN_FLOAT) for char in text[1:-1])
-        print prob_m
-        return _prob_start + _prob_trans + prob_m + _prob_trans_MM * (len_ - 3) + prob_b + prob_e
-
-
+    return prob_b + prob_e 
