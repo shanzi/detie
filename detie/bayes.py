856ef4713aa8b3b28d67a82eba36f59cc4a4938e
Chase Zhang <yun.er.run@gmail.com>
2013-12-10 19:54:01 +0800

    add positive prior

--- detie/bayes.py
+++ detie/bayes.py
@@ -12,7 +12,7 @@ def train(spam_words, unlabeled_words):
     spams = list(map(features, spam_words))
     unlabeled = list(map(features, unlabeled_words))
 
-    model = PositiveNaiveBayesClassifier.train(spams, unlabeled)
+    model = PositiveNaiveBayesClassifier.train(spams, unlabeled, 0.9)
     data = PickleData('bayesmodel.pickle')
     data.write(model)
     return model
